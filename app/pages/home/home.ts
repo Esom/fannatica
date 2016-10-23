cc2f65db2ea078d25a53136c36b89bea0b84a30d
esom <mailesom@gmail.com>
2016-10-23 19:32:43 +0100

    menu + tabs

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
+import { FacebookPage } from '../facebook/facebook';
+import { TwitterPage } from '../twitter/twitter';
+import { InstagramPage } from '../instagram/instagram';
+import { YoutubePage } from '../youtube/youtube';
+import { SoundcloudPage } from '../soundcloud/soundcloud';
+import { StorePage } from '../store/store';
 
-/*
-  Generated class for the HomePage page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   templateUrl: 'build/pages/home/home.html',
 })
 export class HomePage {
+  	// set the root pages for each tab
+	tab1Root = SoundcloudPage;
+	tab2Root = FacebookPage;
+	tab3Root = InstagramPage;
+	tab4Root = TwitterPage;
+	tab5Root = YoutubePage;
+	tab6Root = StorePage;
 
-  constructor(private navCtrl: NavController) {
 
-  }
+	constructor(private navCtrl: NavController) {
 
+	}
 }
